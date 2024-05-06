75215677fa94ed49dfd7662077cf23fd4f5a013b
Robert Richter <50635122+rrcomtech@users.noreply.github.com>
2024-05-06 09:28:44 +0200

    [Data] Harvesting Data of Starlink in RIPE ATLAS (#49)

---
+++ src/harvester/producers/ripe-prod/src/main.ts
@@ -0,0 +1,128 @@
+import KafkaProducer from "./kafka-producer";
+import { ROOTSERVERS_BUILTIN_PING, ROOTSERVER_BUILTIN_TRACEROUTE } from "./root_server_config";
+import { Probe, ProbeStatus, string_to_probestatus } from "./util";
+import WebSocket from "ws";
+
+const URL = "https://atlas.ripe.net/api/v2/";
+const ASN = 14593;
+
+const ripe_up = async () => {
+	const response = await fetch(URL);
+	if (response.status !== 200) {
+		return false;
+	}
+	return true;
+};
+
+/**
+ * Get all probes on RIPE ATLAS that have a Starlink ASNv4.
+ * For each probe, a Probe Object is created.
+ *
+ * @returns
+ * 		Returns a list of probes.
+ */
+const get_starlink_probes = async () => {
+	const url = URL + "probes/?asn_v4=" + ASN;
+	let response = await (await fetch(url)).json();
+
+	const probes: Probe[] = [];
+	while (probes.length < response.count) {
+		for (const probe of response.results) {
+			probes.push({
+				country: probe.country_code,
+				geometry: probe.geometry.coordinates,
+				id: probe.id,
+				status: string_to_probestatus(probe.status.name),
+				ipv4: probe.address_v4,
+			});
+		}
+
+		if (response.next) {
+			response = await (await fetch(response.next)).json();
+		} else {
+			break;
+		}
+	}
+
+	return probes;
+};
+
+const main = async () => {
+	if (!(await ripe_up())) {
+		console.error("RIPE ATLAS is down.");
+		process.exit(1);
+	}
+
+	const probes = await get_starlink_probes();
+	let probe: Probe;
+	for (let i = 0; i < probes.length; ++i) {
+		if (probes[i].status === ProbeStatus.CONNECTED) {
+			probe = probes[i];
+			break;
+		}
+	}
+
+	let stream_configs: any[] = [];
+
+	// TODO: Add sendBacklog parameter in order to cover the last few minutes of possible disconnects.
+	for (const probe of probes) {
+		for (const server of ROOTSERVERS_BUILTIN_PING) {
+			const param = {
+				streamType: "result",
+				msm: server,
+				type: "ping",
+				prb: probe.id,
+			};
+			stream_configs.push(param);
+		}
+	}
+
+	for (const probe of probes) {
+		for (const server of ROOTSERVER_BUILTIN_TRACEROUTE) {
+			const param = {
+				streamType: "result",
+				msm: server,
+				type: "traceroute",
+				prb: probe.id,
+			};
+			stream_configs.push(param);
+		}
+	}
+
+	console.log(`Got a total of ${stream_configs.length} stream configurations.`);
+
+	// Wait for Kafka to be ready and finished its dumb leadership election.
+	await new Promise(resolve => setTimeout(resolve, 20000));
+
+	const kafka_producer = new KafkaProducer("measurements");
+	await kafka_producer.connect();
+
+	const socket = new WebSocket("wss://atlas-stream.ripe.net/stream/");
+	socket.onmessage = (event: any): void => {
+		const [type, payload] = JSON.parse(event.data);
+
+		if (type === "atlas_error") {
+			console.warn(payload);
+		}
+
+		if (type === "atlas_result") {
+			kafka_producer.send(JSON.stringify(payload));
+		}
+	};
+
+	socket.onopen = async (_: any): Promise<void> => {
+		for (const config of stream_configs) {
+			socket.send(JSON.stringify(["atlas_subscribe", config]));
+			// Rate limit to 1 subscription per second.
+			await new Promise(resolve => setTimeout(resolve, 500));
+		}
+		console.log("Subscribed to all builtin Starlink Measurements.");
+	};
+
+	process.on('exit', () => {
+		socket.close();
+		kafka_producer.disconnect();
+	});
+};
+
+main();
---
+++ src/harvester/producers/ripe-prod/src/util.ts
@@ -0,0 +1,45 @@
+interface Measurement {
+	measurement_id: number;
+	probe_id: number;
+	timestamp: number;
+	from: string;
+	to: string;
+	protocol: string;
+	type: string;
+	group_id: string;
+	stored_timestamp: number;
+	qbuf: string;
+	result: Object;
+}
+
+enum ProbeStatus {
+	CONNECTED = 'Connected',
+	DISCONNECTED = 'Disconnected',
+	ABANDONED = 'Abandoned',
+}
+
+const string_to_probestatus = (status: string): ProbeStatus => {
+	switch (status) {
+		case 'Connected':
+			return ProbeStatus.CONNECTED;
+		case 'Disconnected':
+			return ProbeStatus.DISCONNECTED;
+		default:
+			return ProbeStatus.ABANDONED;
+	}
+};
+
+interface Probe {
+	country: string;
+	geometry: number[];
+	id: number;
+	status: ProbeStatus;
+	ipv4: string;
+}
+
+export {
+	ProbeStatus,
+	Probe,
+	string_to_probestatus,
+	Measurement,
+};
