a8b11e02c2fd33ad81b05a2e9f36eb65d891f5f6
Robert Richter <robert.richter@rrcomtech.com>
2024-06-03 16:14:43 +0200

    State

--- src/harvester/producers/ripe-prod/src/main.ts
+++ src/harvester/producers/ripe-prod/src/main.ts
@@ -1,4 +1,5 @@
 import assert from "assert";
+import fs from 'fs';
 import { ROOTSERVERS_BUILTIN_PING } from "./root_server_config";
 import { START_TIMESTAMP, STOP_TIMESTAMP } from "./timeframe_config";
 import { Probe, string_to_probestatus } from "./util";
@@ -7,7 +8,8 @@ import { StoreController } from "./store.controller";
 
 const URL = "https://atlas.ripe.net/api/v2/";
 const ASN = 14593;
-const TIMEFRAME = 60 * 60 * 24; // 1 day; Feasible timeframe as tested in Postman.
+const TIMEFRAME = 60 * 60 * 24; // 1 day in seconds
+const MAX_MEMORY = 8192;  // in MB
 
 let db: StoreController;
 
@@ -76,12 +78,14 @@ const download_and_store = async (chunk: ProbeServerPair[]) => {
 				try {
 					const response = await fetch(url);
 					if (response.status === 429) {
-						await new Promise((resolve) => setTimeout(resolve, 5000));
+						await new Promise((resolve) => setTimeout(resolve, 2000));
 						return await fetch_data(url);
 					}
-					return response;
+					return response
 				} catch (error) {
-					await new Promise((resolve) => setTimeout(resolve, 5000));
+					{
+						await new Promise((resolve) => setTimeout(resolve, 5000));
+					}
 					return await fetch_data(url);
 				}
 			};
@@ -89,22 +93,23 @@ const download_and_store = async (chunk: ProbeServerPair[]) => {
 
 			if (response.status !== 200) {
 				console.error(`Failed to fetch data from ${url}`);
-				continue;
-			}
-
-			// For some reason, some data points return an empty array.
-			const data = await response.json();
-			if (data.length === 0) {
-				//console.error(`No data found for probe ${probe_id} and server ${server}. URL: ${url}`);
 			} else {
-				// Sending points individually, as the whole body might be too large.
-				for (let point of data) {
-					point.source_platform = "RIPE ATLAS (builtin)";
+				// For some reason, some data points return an empty array.
+				const data = await response.json();
+				if (data.length === 0) {
+					//console.error(`No data found for probe ${probe_id} and server ${server}. URL: ${url}`);
+				} else {
+					// Sending points individually, as the whole body might be too large.
+					for (let point of data) {
+						point.source_platform = "RIPE ATLAS (builtin)";
+					}
+					parentPort.postMessage(data);
 				}
-				parentPort.postMessage(data);
-			}
+			};
 			current_timestamp += TIMEFRAME;
-		};
+			// Ethical crawling
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+		}
 	}
 };
 
@@ -118,22 +123,24 @@ const main = async (threads = 1) => {
 	assert(START_TIMESTAMP < STOP_TIMESTAMP, "Invalid timeframe");
 	assert(STOP_TIMESTAMP < Date.now(), "Stop Timestamp is in the future. Choose something from the past.");
 
-	const probes: Probe[] = await get_starlink_probes();
-	let probe_server_pair: ProbeServerPair[] = [];
-	for (const server of ROOTSERVERS_BUILTIN_PING) {
-		for (const probe of probes) {
-			probe_server_pair.push({ server, probe });
-		}
-	}
-
+	// Splits the probe-server pairs into individual chunks.
 	const chunks = [];
-	const chunk_size = Math.ceil(probe_server_pair.length / threads);
-	for (let i = 0; i < probe_server_pair.length; i += chunk_size) {
-		chunks.push(probe_server_pair.slice(i, i + chunk_size));
-	}
+	{
+		const probes: Probe[] = await get_starlink_probes();
+		let probe_server_pair: ProbeServerPair[] = [];
+		for (const server of ROOTSERVERS_BUILTIN_PING) {
+			for (const probe of probes) {
+				probe_server_pair.push({ server, probe });
+			}
+		}
 
-	console.log(`Got a total of ${probe_server_pair.length} probe-server pairs split into ${chunks.length} chunks.`);
-	console.log(`Length of first chunk: ${chunks[0].length}`);
+		const chunk_size = Math.ceil(probe_server_pair.length / threads);
+		for (let i = 0; i < probe_server_pair.length; i += chunk_size) {
+			chunks.push(probe_server_pair.slice(i, i + chunk_size));
+		}
+		console.log(`Got a total of ${probe_server_pair.length} probe-server pairs split into ${chunks.length} chunks.`);
+		console.log(`Length of first chunk: ${chunks[0].length}`);
+	}
 
 	for (const chunk of chunks) {
 		// Spawns a new worker that will download and send the data.
@@ -144,9 +151,23 @@ const main = async (threads = 1) => {
 	}
 };
 
+const print_memory_usage = async () => {
+	{
+		await (new Promise((resolve) => setTimeout(resolve, 1000)));
+		const memory_usage = process.memoryUsage().heapUsed / 1024 / 1024;
+		process.stdout.write("  " + Math.round(memory_usage) + " MB\r");
+		fs.appendFileSync('memory_leak.csv', `${Date.now()};${Math.round(memory_usage)}\n`);
+
+	}
+	print_memory_usage();
+};
+
 if (isMainThread) {
+	fs.writeFileSync('memory_leak.csv', 'timestamp;memory\n');
+
 	db = new StoreController();
 	main(256);
+	print_memory_usage();
 } else {
 	download_and_store(workerData);
 }
--- src/harvester/storage-engine/src/storage.config.ts
+++ src/harvester/storage-engine/src/storage.config.ts
@@ -9,14 +9,10 @@ const DB_CONFIG = {
 const DROP_TABLES = true;
 const DROP_QUERIES = `
   DROP TABLE IF EXISTS ping_data;
-  DROP SEQUENCE IF EXISTS ping_data_id_seq;
 `;
 
 const SETUP_QUERIES = `
-  CREATE SEQUENCE ping_data_id_seq START 1;
-
   CREATE TABLE IF NOT EXISTS ping_data (
-    id INTEGER PRIMARY KEY DEFAULT nextval('ping_data_id_seq'),
     msm_id UINTEGER,
     destination VARCHAR,
     source VARCHAR,
