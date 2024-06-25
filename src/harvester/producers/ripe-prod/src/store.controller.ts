28c8948e0ac64689deff99f40b444a8eb194b034
Robert Richter <robert.richter@rrcomtech.com>
2024-06-25 11:23:41 +0200

    Added try-catch

--- src/harvester/producers/ripe-prod/src/store.controller.ts
+++ src/harvester/producers/ripe-prod/src/store.controller.ts
@@ -1,21 +1,28 @@
 const API: string = 'http://storageengine:8001';
 
 const storeData = async (data: object, endpoint: string) => {
-  const body = JSON.stringify(data);
-  const response = await fetch(`${API}/${endpoint}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: body,
-  });
+  try {
+    const body = JSON.stringify(data);
+    const response = await fetch(`${API}/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: body,
+    });
 
-  // POST requests to NestJS always yield a 201 status code by default.
-  if (response.status !== 201) {
-    console.error('Failed to store data.');
-    console.error(response.statusText);
-    console.error(await response.text());
-    process.exitCode = 1;
+    // POST requests to NestJS always yield a 201 status code by default.
+    if (response.status !== 201) {
+      console.error('Failed to store data.');
+      console.error(response.statusText);
+      console.error(await response.text());
+      process.exitCode = 1;
+    }
+  } catch (e) {
+    // If error occurrs here for some reason, wait a bit and retry afterwards.
+    console.warn('Failed to send data to database. Retrying ...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await storeData(data, endpoint);
   }
 };
 
