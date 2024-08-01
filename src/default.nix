5587b0b15173c977254abd3ed62e1e977b04749f
Robert Richter <50635122+rrcomtech@users.noreply.github.com>
2024-08-01 09:26:49 +0200

    [Infrastructure] Adding checkers to import shell (#149)
    Adds some useful commands for the nix-shell

--- src/default.nix
+++ src/default.nix
@@ -1,8 +1,3 @@
-#
-# Usage:
-#   nix-shell --run "runAll"
-#
-
 let
   pkgs = import ../nixpkgs {};
 in pkgs.mkShell {
@@ -15,7 +10,7 @@ in pkgs.mkShell {
   ];
 
   shellHook = ''
-    runDockerCompose() {
+    runBackend() {
       tmux new-session -d -s dbbackend -c $(pwd)/db-controller
       tmux send 'newgrp docker' ENTER # For some reason this is required
       tmux send 'docker-compose up --build' ENTER
@@ -28,23 +23,53 @@ in pkgs.mkShell {
     }
 
     runAll() {
-      runDockerCompose
+      runBackend
       sleep 30 # Wait for the backend to start
       runProducers
+    }
+
+    killProducers() {
+      tmux kill-session -t producers
+    }
+
+    killBackend() {
+      tmux send-keys -t dbbackend C-c
+      # It takes up to 10.2s for Docker to shut down their containers.
+      sleep 15
+      tmux kill-session -t dbbackend
+    }
+
+    killAll() {
+      killProducers
+      killBackend
     }
 
     help() {
       echo ""
       echo "Welcome to the producers shell 🚀 !"
       echo ""
+      echo "Be aware, you need to be in the /src dir of the repository outside a tmux session."
       echo "Following commands are available:"
-      echo "   - runDockerCompose: Start the backend services in the 'dbbackend' tmux session."
+      echo "   - runBackend: Start the backend services in the 'dbbackend' tmux session."
       echo "   - runProducers: Start the producers in the 'producers' directory."
       echo "   - runAll: Execute the previously two mentioned commands with a sleep of 30s in between."
+      echo "   - killAll: Kill 'dbbackend' and 'producers' tmux sessions."
+      echo "   - killBackend: Cancels processes and jills 'dbbackend' tmux session."
+      echo "   - killProducers: Kills 'producers' tmux session."
       echo "   - help: Display this help message."
     }
 
-    help
+    if [ "$TERM_PROGRAM" = "tmux" ]; then
+      echo "Cannot run tmux scripts in tmux session."
+      echo "Exiting ..."
+      exit
+    else
+      curr_dir=$(basename $(pwd))
+      if [ ! "$curr_dir" = "src" ]; then
+        cd src
+      fi
+      help
+    fi
   '';
 }
 
