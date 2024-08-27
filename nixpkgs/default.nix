269768fa80efe92318748a2ffa99e4f547a6880c
Robert Richter <50635122+rrcomtech@users.noreply.github.com>
2024-08-27 13:43:28 +0200

    [Nix] Switch to Nix 24.05 (#168)
    Switch from master branch to nixos-24.05 (most recent release).

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -2,13 +2,14 @@
 
 let
   hashes = import ./hashes.nix;
-  fetchCommit = commit: fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
-    ${if hashes ? "${commit}" then "sha256" else null} = hashes.${commit}.hash;
+  ref = "nixos-24.05";
+  hash = "0q96nxw7jg9l9zlpa3wkma5xzmgkdnnajapwhgb2fk2ll224rgs1";
+
+  fetchCommit = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${ref}.tar.gz";
+    sha256 = "${hash}";
   };
 in
 
 # Keep line 12 as it is! Only modify it by running update-default-nix.py. The updating script depends on the next line being as it is on line 12.
-import (fetchCommit "797f7dc49e0bc7fab4b57c021cdf68f595e47841") {
-
-}
+import fetchCommit { }
